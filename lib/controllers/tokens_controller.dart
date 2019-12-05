1a86e56f1f46955cd40876d9db014de9dfd0dadb
Crazy Genius <elyudde@gmail.com>
2019-12-05 14:18:02 +0300

    update

--- lib/controllers/tokens_controller.dart
+++ lib/controllers/tokens_controller.dart
@@ -10,6 +10,12 @@ class CooprateTokenController extends ResourceController{
   static int duration = 300; // 300 seconds for 5 mins
   final int _validTill = (DateTime.now().millisecondsSinceEpoch/1000 + duration).floor();
 
+  String _requestId;
+  final ResposeType _responseType = ResposeType.baseUser;
+  ResponsesStatus _responseStatus;
+  dynamic _responseBodyModel;
+  Map<String, dynamic> _responseBody;
+
   @Operation.get()
   Future<Response> getCooprateToken()async{
     // Save Request
@@ -34,16 +40,22 @@ class CooprateTokenController extends ResourceController{
     final Map<String, dynamic> _dbRes = await _tokenModel.save();
 
     if(_dbRes['status'] == 0){
-        return Response.ok({
+        _responseStatus = ResponsesStatus.success;
+      _responseBody = {
           "status": 0,
           "body": {
             "token": _tokenModel.token,
             "validTill": _validTill,
           }
-        });
+        };
     } else {
-      return Response.badRequest(body: {"status": 1, "body": "an error occured."});
+      _responseStatus = ResponsesStatus.failed;
+      _responseBody = {"status": 1, "body": "an error occured."};
     }
+    // Save response
+    final ResponsesModel _responsesModel = ResponsesModel(requestId: _requestId, responseType: _responseType, status: _responseStatus, responseBody: _responseBodyModel != null ? _responseBodyModel : _responseBody);
+    unawaited(_responsesModel.save());
+    return _responsesModel.sendResponse(_responseBody);
     
   }  
     
